817d48b72263631a7875afb59e56e95973877829
manoj-codes <66223649+manoj-codes@users.noreply.github.com>
2024-05-05 21:37:29 -0400

    Update ApiClientInterface.php

--- Contracts/ApiClientInterface.php
+++ Contracts/ApiClientInterface.php
@@ -4,13 +4,5 @@ namespace App\ApiClients\Contracts;
 
 interface ApiClientInterface
 {
-    /**
-     * Send a request to the specified endpoint using the provided HTTP method and data.
-     *
-     * @param string $method The HTTP method to use for the request.
-     * @param string $url The endpoint URL to send the request to.
-     * @param array $data The data to send with the request, if any.
-     * @return array The API response as an associative array.
-     */
-    public function sendRequest($method, $url, $data = []);
+    public function sendRequest(string $method, string $url, array $data = []): array;
 }
